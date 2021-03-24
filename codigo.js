4a4a04753203408d5866d77b6c586d301b2c857c
Orlando Martinez <omjuniormusic@gmail.com>
2021-03-24 19:01:08 -0300

    Trabalhando em Codigo de Validação

--- codigo.js
+++ codigo.js
@@ -1,11 +1,19 @@
 let envio = document.getElementById("submit");
-let nombre = document.getElementById("nombres");
-
+var nombre = document.getElementById("nombres");
+let apellido = document.getElementById("apellidos");
+/*
 envio.addEventListener("click", (event) => {
     event.preventDefault()
-    if (nombre.value === "") {
+    if (nombre.value === "" || nombre.value.length<1) {
         document.getElementById('warnings').innerHTML = 'Escribe tu nombre';
         nombre.focus();
     } 
 })
+*/
+function contarCaracteres(){
+    if (nombre.value.length < 1) {
+        alert('Campo vacío');
+    }
+}
 
+  
